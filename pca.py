cbc4a913b5537170b84f39cb435852fc7cde5877
Mehmet Onur Erboy <mehmetonurerboy67@gmail.com>
2019-10-31 00:00:32 +0300

    Thresold value calculation side is added
    For calculating threshold value, some fields are calculated like :
    
    - eig_value_sum
    - eig_value_sum2
    - eig_value_sum3
    - h0
    - c_alpha
    - threshold_value

--- pca.py
+++ pca.py
@@ -32,3 +32,24 @@ def anomalyDetectionWithPCA(dataFrame, selected_column_names):
 
     print(w)
     print(v)
+
+    eig_value_sum = 0
+    eig_value_sum2 = 0
+    eig_value_sum3 = 0
+
+    for indis in range(len(selected_column_names)):
+        eig_value_sum += w[indis]
+        eig_value_sum2 += w[indis]**2
+        eig_value_sum3 += w[indis]**3
+
+    print(eig_value_sum)
+    print(eig_value_sum2)
+    print(eig_value_sum3)
+
+    h0 = 1 - ((2*eig_value_sum*eig_value_sum3) / (3*(eig_value_sum2**2)))
+    print(h0)
+
+    c_alpha = 0.9
+
+    threshold_value = (eig_value_sum*(((c_alpha*(2*eig_value_sum2*h0**2)**(1/2))/eig_value_sum) + 1 + ((eig_value_sum2*h0*(h0-1))/(eig_value_sum**2)))**(1/h0))**(1/2)
+    print(threshold_value)
