2303c582f23fcad6d83c4325ec224328967ccbc3
Mehmet Onur Erboy <mehmetonurerboy67@gmail.com>
2019-10-31 22:14:00 +0300

    Outlier detect part is added.
    - obtainY_values function -> split an array to matrix (this entered value is actually a matrix but it is represented with 1D array. We transform this to 2D array.)
    
    - detectAnomalyValueIndices -> this function detects the anomaly points. (According to paper's formula)
    
    - calculateLengthOfVirtualPoint -> this function calculates the length of virtual points according to center for detecting anomalies.
    
    - THERE ARE SOME PROBLEMS ON CALCULATIONS. THEY WILL BE FIXED

--- pca.py
+++ pca.py
@@ -19,9 +19,45 @@ def getCorrelationMatrix(dataFrame, inputCols) :
 
     values = matrix.collect()[0]["pearson({})".format(vector_col)].values
     matrix = np.reshape(np.array(values), (-1, 4))
-
-
     return matrix
+
+def obtainY_values(dataFrame, selected_column_names) :
+    queried_values = dataFrame.select(selected_column_names).rdd.flatMap(lambda x : x).collect()
+
+    print(queried_values)
+    queried_matrix = np.reshape(np.array(queried_values), (-1, 4))
+    print(queried_matrix)
+
+    return queried_matrix
+
+def detectAnomalyValueIndices(dataFrame, y_value_matrix, threshold_value, projection_matrix):
+    return_values = []
+
+    for indis in range(len(y_value_matrix)):
+        spe_value = calculateLengthOfVirtualPoint(np.dot(projection_matrix,y_value_matrix[indis].T))
+        print('[' + str(indis) + '] : ' + str(spe_value) + '\t threshold : ' + str(threshold_value))
+        if (spe_value > threshold_value):
+            return_values.append(y_value_matrix[indis])
+
+    return return_values
+
+def calculateLengthOfVirtualPoint(pointValues):
+    value = 0
+    for indis in range(len(pointValues)):
+        value += pointValues[indis]**2
+    return value
+
+def findSequenceOfEigenValues(eigen_value_array):
+    indices = []
+    for indis in range(len(eigen_value_array)):
+        maxValue = eigen_value_array[indis]
+        maxIndis = indis
+        for indis2 in range(len(eigen_value_array)-1):
+            if eigen_value_array[indis2] > eigen_value_array[indis]:
+                maxIndis = indis2
+                maxValue = eigen_value_array[indis2]
+        indices.append(maxIndis)
+
 def anomalyDetectionWithPCA(dataFrame, selected_column_names):
     print(selected_column_names)
     correlation_matrix = getCorrelationMatrix(dataFrame, selected_column_names)
@@ -53,3 +89,26 @@ def anomalyDetectionWithPCA(dataFrame, selected_column_names):
 
     threshold_value = (eig_value_sum*(((c_alpha*(2*eig_value_sum2*h0**2)**(1/2))/eig_value_sum) + 1 + ((eig_value_sum2*h0*(h0-1))/(eig_value_sum**2)))**(1/h0))**(1/2)
     print(threshold_value)
+
+    print(w)
+    print(np.eye(4))
+    print('\n\n')
+    print(v)
+    print('\n\n')
+    print(v.T)
+    print('\n\n')
+    print(np.dot(v,v.T))
+    projection_matrix = np.subtract(np.eye(4),(np.dot(v,v.T)))
+    print(projection_matrix)
+
+    #suspected_values = [[]]
+
+    y_values = obtainY_values(dataFrame,selected_column_names)
+
+    print(len(y_values))
+    print(dataFrame.count())
+
+    outlier_values = detectAnomalyValueIndices(dataFrame, y_values, threshold_value, projection_matrix)
+    print(outlier_values)
+    print(len(outlier_values))
+
